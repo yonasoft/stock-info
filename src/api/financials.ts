499b9f1173aa81bb7471dc09ab58fffee8600582
KevC000 <chenkevin1119@gmail.com>
2024-06-06 13:59:05 -0400

    added would be code if there weren't CORS issue with api from the docker image

--- src/api/financials.ts
+++ src/api/financials.ts
@@ -9,3 +9,25 @@ export const getFinancials = async (): Promise<FinancialData> => {
     }, 1000);
   });
 };
+
+
+//This is the code that would be used to fetch data from the API
+
+// const baseURL = "http://127.0.0.1:8000";
+// const apiClient = axios.create({
+//   baseURL,
+// });
+
+// export const getFinancials = async () => {
+//   try {
+//     const response = await apiClient.get("/fianancials", {
+//       params: {},
+//     });
+//     console.log(response.data);
+
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching data", error);
+//     throw new Error("No cached data available");
+//   }
+// };
