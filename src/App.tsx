0110667eb4db70e589e926a79b6a1396e9a420c8
KevC000 <chenkevin1119@gmail.com>
2024-06-05 23:06:45 -0400

    adjusted coloring

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import StockInformation from "./pages/StockInformation";
 import Header from "./components/Header";
 function App() {
   return (
-    <div className="App flex flex-col h-screen bg-gray-700  text-white overflow-y-scroll overflow-x-clip">
+    <div className="App flex flex-col h-screen bg-gray-800  text-white overflow-y-scroll overflow-x-clip">
       <Header />
       <div className="lg:container h-full mx-auto ">
         <StockInformation ticker={"AAPL"} />
