4d932eccfe769de58e53183ab6afc35659fd4db7
KevC000 <chenkevin1119@gmail.com>
2024-06-06 16:32:20 -0400

    slight changed for readability

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,7 @@ import React from "react";
 // Header component for the app
 const Header = () => {
   return (
-    <div
-      className="
-  		flex flex-col items-center justify-center h-24 bg-gray-900 text-white text-2xl font-bold shadow-lg"
-    >
+    <div className="flex flex-col items-center justify-center h-24 bg-gray-900 text-white text-2xl font-bold shadow-lg">
       <h1>Stock Market Overview</h1>
     </div>
   );
